c7855c892ca15459a8ada654ccbd2a935c033759
summer <167855067+summerapy@users.noreply.github.com>
2024-11-25 17:48:17 +0900

    Update webserver1.js

--- webserver1.js
+++ webserver1.js
@@ -8,6 +8,13 @@ server.on('request', (req, res) => {
     const filename = "week3.html"
     fs.readFile(filename, (err, data) => {
         res.write(data);
+        if (err = null) {
+            res.write(data);
+            res.end();
+        }
+        else {
+            res.end("File Not Found");
+        }
     });
     res.end('<h1>Hello World!</h1>');
     });
