90f896a88cb320570bc85c3db89d084c235d5b5b
summer <167855067+summerapy@users.noreply.github.com>
2024-11-25 17:42:09 +0900

    Add files via upload

---
+++ webserver1.js
@@ -0,0 +1,17 @@
+const http = require('http');
+const fs = require('fs');
+
+const server = http.createServer();
+
+server.on('request', (req, res) => {
+    res.writeHead(200, {'Content-Type': 'text/html'});
+    const filename = "week3.html"
+    fs.readFile(filename, (err, data) => {
+        res.write(data);
+    });
+    res.end('<h1>Hello World!</h1>');
+    });
+
+
+
+server.listen(500);
